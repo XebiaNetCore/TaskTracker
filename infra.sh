2011d8ac46bf498e63e35f7f919dffa4f161d81a
konarpriyanku <konar.priyanku@gmail.com>
2018-04-17 17:05:13 +0530

    added test pproject templates

--- infra.sh
+++ infra.sh
@@ -4,6 +4,8 @@
 ### Creating the application infrastructure using shell script ###
 ###################################
 
+
+
 addProjectReference(){
 
     dotnet add "$1" reference "$2"
@@ -23,7 +25,11 @@ createSolution()
     
     cd src
     dotnet new webapi -n "$1.Api"
-    dotnet new webapi -n "$1.Services.Identity"
+    dotnet new webapi -n "$1.Services.Identi
+log()
+{
+    echo "$1" 
+}ty"
     dotnet new webapi -n "$1.Services.Tasks"
     dotnet new classlib -n "$1.Common"
 
@@ -36,18 +42,27 @@ createSolution()
         addProjectReference "$1.Services.Tasks/$1.Services.Tasks.csproj" reference $1.Common/$1.Common.csproject
     cd ..
     cd tests
-    dotnet new webapi -n "$1.Api.Test"
-    dotnet new webapi -n "$1.Services.Identity.Test"
-    dotnet new webapi -n "$1.Services.Tasks.Test"
+    dotnet new xunit -n "$1.Api.Test"
+    dotnet new xunit -n "$1.Services.Identity.Test"
+    dotnet new xunit -n "$1.Services.Tasks.Test"
+
+
+        log "Adding projects to solution ...."
+    cd ..
+    dotnet addn sln "src/$1.Api/$1.Api.csproj"
+    dotnet addn sln "src/$1.Api.Common/$1.Api.Common.csproj"
+    dotnet addn sln "src/$1.Api.Services.Identity/$1.Api.Services.Identity.csproj"
+    dotnet addn sln "src/$1.Api.Services.Tasks/$1.Api.Services.Tasks.csproj"
+
 
 }
-
-
-
 log()
 {
     echo "$1" 
 }
+
+
+
 
  SOLN="";
 if [ $# > 0 ]; then 
